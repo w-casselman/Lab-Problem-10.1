95a7f687ad6547bf168524f2eed5b2d2f43c1fba
Wesley Casselman <w-casselman@onu.edu>
2023-11-02 09:10:21 -0400

    count_letters Function Working

--- P10-1/P10-1.cpp
+++ P10-1/P10-1.cpp
@@ -2,3 +2,28 @@
 // Wesley Casselman
 // All Souls' Day of 2023
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int count_letters(string str);
+
+int main(void)
+{
+	cout << count_letters("asdf.ah/.filyifsdf");
+
+	return 0;
+}
+
+int count_letters(string str)
+{
+	int letters = 0;
+	
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z')
+			letters++;
+	}
+	return letters;
+}
